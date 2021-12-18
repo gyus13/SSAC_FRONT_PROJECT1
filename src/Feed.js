0c690f5fb5ad2d173c05b6ac4ce342d13360c446
gyus13 <65529348+gyus13@users.noreply.github.com>
2021-12-18 12:45:04 +0900

    feed 수정

--- src/Feed.js
+++ src/Feed.js
@@ -9,26 +9,26 @@ const Feed = () => {
     return (
         <div className="feed">
             <Navbar></Navbar>
-            <div className="header"><b>구로구 맛집탐방</b></div>
-            <hr />
-            <div className="category">
+            <div class="header"> 헤더부분 </div>
+                    <hr />
+                    <div className="category">
 
-            <Link to="/feed"><div>게시물</div></Link>
-            <Link to="/map"><div>지도</div></Link>
-            
-            </div>
-            <Route path="/feed">
-                {
-                    stores.map((a, i) => {
-                        return <Item stores={stores[i]}></Item>
-                    })}
-            </Route>
+                        <Link to="/feed"><div>게시물</div></Link>
+                        <Link to="/map"><div>지도</div></Link>
 
-            <Route path="/map">
-                <p>지도 페이지 입니다.</p>
-            </Route>
-        </div>
-    )
+                    </div>
+                    <Route path="/feed">
+                        {
+                            stores.map((a, i) => {
+                                return <Item stores={stores[i]}></Item>
+                            })}
+                    </Route>
+
+                    <Route path="/map">
+                        <p>지도 페이지 입니다.</p>
+                    </Route>
+                </div>
+                )
 }
 
-export default Feed;
+                export default Feed;
