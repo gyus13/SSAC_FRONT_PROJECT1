45666a80333c7c711c1bbe3b494ec6d35bd42e76
오효영 <96075770+hyoyoung-OH@users.noreply.github.com>
2021-12-20 17:13:36 +0900

    팔로우 버튼

--- src/Feed.js
+++ src/Feed.js
@@ -1,31 +1,33 @@
 import "./Feed.css";
 
 const Feed = () => {
-    return (
-        <div className="Feed">
-            <div className ="header"><b>구로구 맛집탐방</b></div>
-        <hr/>
-        <div className="Category">
-            <div>게시물</div>
-            <div>지도</div>
-        </div>
-        <div className="Feed-container">
-           <div className="item"></div> 
-           <div className="item"></div> 
-           <div className="item"></div> 
-        </div>
-        <div className="Feed-container">
-           <div className="item"></div> 
-           <div className="item"></div> 
-           <div className="item"></div> 
-        </div>
-        <div className="Feed-container">
-           <div className="item"></div> 
-           <div className="item"></div> 
-           <div className="item"></div> 
-        </div>
-        </div>
-    )
-}
+  return (
+    <div className="Feed">
+      <div className="header">
+        <b>구로구 맛집탐방</b>
+      </div>
+      <hr />
+      <div className="Category">
+        <div>게시물</div>
+        <div>지도</div>
+      </div>
+      <div className="Feed-container">
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+      </div>
+      <div className="Feed-container">
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+      </div>
+      <div className="Feed-container">
+        <div className="item"></div>
+        <div className="item"></div>
+        <div className="item"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Feed;
