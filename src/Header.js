1a7e1dc040568e319811a4f58588644e54b0d718
오효영 <96075770+hyoyoung-OH@users.noreply.github.com>
2021-12-20 22:42:57 +0900

    버튼

--- src/Header.js
+++ src/Header.js
@@ -1,59 +1,10 @@
-// import { useState } from "react";
-// import "./App.css";
-// import Modal from "react-modal";
-
-// const Header = () => {
-//   const [follow, setFollow] = useState(369369);
-//   const onClick = () => setFollow(follow + 1);
-
-//   return (
-//     <div className="header">
-//       <div className="header__profile">
-//         <div className="header__profileimgwrapper">
-//           <div className="header__profileimgwrapper-profileimg">
-//             <img
-//               src="profileimg_amil.jpg"
-//               width="200"
-//               height="200"
-//               alt="프로필 이미지"
-//             ></img>
-//           </div>
-//         </div>
-
-//         <div className="header__profile-description">
-//           <h1>군싹이</h1>
-//           <button onClick={onClick}>팔로우</button>
-
-//           <Modal isOpen={false}>모달달달</Modal>
-
-//           <span>
-//             <b>게시물: 10 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 팔로워: {follow}</b>
-//           </span>
-//           <p>
-//             군침이 싸악 도는 한국의 모든 맛집 탐구!
-//             <br />
-//             양식, 중식, 한식, 분식, 일식 등 가리지 않아요
-//             <br />
-//             🍲 🍛
-//           </p>
-//         </div>
-//       </div>
-
-//       <div className="header__link">
-//         <div className="header__link-container"></div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Header;
-
 import { useState } from "react";
 import "./App.css";
 import Modal from "react-modal";
 
 const Header = () => {
-  const [follow, setFollow] = useState(369369);
+  // const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [follow, setFollow] = useState(0);
   const [isClicked, setIsClicked] = useState(false);
   const onClick = () => {
     if (isClicked) {
@@ -82,8 +33,13 @@ const Header = () => {
         <div className="header__profile-description">
           <h1>군싹이</h1>
           <button onClick={onClick}>{isClicked ? "팔로잉" : "팔로우"}</button>
-
-          <Modal isOpen={false}>모달달달</Modal>
+          {/* <button onClick={() => setModalIsOpen(true)}>더보기</button>
+          <Modal isOpen={true}>
+            <div>메세지 보내기</div>
+            <div>사용자 차단</div>
+            <div>취소</div>
+            <button onClick={() => setModalIsOpen(false)}>더보기</button>
+          </Modal> */}
 
           <span>
             <b>게시물: 10 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 팔로워: {follow}</b>
@@ -93,7 +49,7 @@ const Header = () => {
             <br />
             양식, 중식, 한식, 분식, 일식 등 가리지 않아요
             <br />
-            🍲 🍛
+            🍲 🍛&#127858; &#127829; &#127828; &#127843; &#127838;
           </p>
         </div>
       </div>
