e81264f910f8c2f8d38c13bc72fd9db3a7a4e4b7
SSOL <livingsnow011@gmail.com>
2021-12-21 12:58:07 +0900

    정리부터

--- src/Component/Map.js
+++ src/Component/Map.js
@@ -2,20 +2,29 @@ import "../App.css"
 import {NaverMap,Marker}  from "react-naver-maps"
 import my_Marker from "../img/my_marker.png";
 import map_Marker from "../img/map_marker.png";
-import { useState } from "react";
 
 const Map = () => {
+
     const navermaps = window.naver.maps;
 
-    //map 함수 묶기 
-    const LatLngs = [(37.47238731740359, 126.89602897456294), (37.468653572531856, 126.89535025189723), (37.46955246261429, 126.89905664026355)
-        (37.4736118303616, 126.89517717590284),(37.47045467981596, 126.89728648444668), (37.47167694499592, 126.90042325184629),
-        (37.4724060587129, 126.89929374026363)
+    //매장 위도,경도 [[배열]]
+    const LatLngs = [
+        [37.468653572531856, 126.89535025189723], 
+        [37.46955246261429, 126.89905664026355],
+        [37.4736118303616, 126.89517717590284],
+        [37.47045467981596, 126.89728648444668], 
+        [37.47167694499592, 126.90042325184629],
+        [37.4724060587129, 126.89929374026363]
     ]
-    //map 묶기
+
+    //확인함수 안지웠음 지워주
+    function chk(){
+        console.log(LatLngs[0][0]);
+    }
 
     return (
         <div className="main">
+            {chk()}
             <NaverMap
                 className="map-container"
                 mapDivId={'map'} // default: react-naver-map
@@ -24,100 +33,40 @@ const Map = () => {
                 height: '85vh' // 네이버지도 세로 길이
                 }}
 
-                defaultCenter={{ lat: 37.47238731740359, lng: 126.89602897456294 }} // 지도 초기 위치
+                defaultCenter={{ lat: 37.47238731740359, lng: 126.89602897456294 }} // 지도 초기 위치 : SSAC
                 defaultZoom={16} // 지도 초기 확대 배율
             >
+                {/* SSac 마커 */}
+                <Marker
+                    position={new navermaps.LatLng(37.47238731740359, 126.89602897456294)}
+                    icon={{
+                        url: my_Marker,
+                        size: new navermaps.Size(50, 50),
+                        origin: new navermaps.Point(0, 0),
+                        anchor: new navermaps.Point(25, 25)
+                    }}
+                />
 
-            <Marker
-                key={0}
-                position={new navermaps.LatLng(37.47238731740359, 126.89602897456294)}
-                icon={{
-                    url: my_Marker,
-                    size: new navermaps.Size(50, 50),
-                    origin: new navermaps.Point(0, 0),
-                    anchor: new navermaps.Point(25, 25)
-                }}
-            >
-            </Marker>
+                {/*매장들 마커 */}
+                {LatLngs.map((LatLng, i) => (
+                    <Marker 
+                        key={i}
+                        position = {new navermaps.LatLng(LatLngs[i][0],LatLngs[i][1])}
+                        icon={{
+                            url: map_Marker,
+                            size: new navermaps.Size(50, 50),
+                            origin: new navermaps.Point(0, 0),
+                            anchor: new navermaps.Point(25, 25)
+                        }}
+                        
+                        // 클릭하면 이동, info 창 생성
+                        // 왜안됨
+                        onClick={() => {
+                            navermaps.panTo(LatLngs[i][0],LatLngs[i][1]);
+                        }}
+                    />))
+                }
 
-
-            <Marker
-                key={1}
-                position={new navermaps.LatLng(37.468653572531856, 126.89535025189723)}
-                icon={{
-                    url: map_Marker,
-                    size: new navermaps.Size(50, 50),
-                    origin: new navermaps.Point(0, 0),
-                    anchor: new navermaps.Point(25, 25)
-                }}
-            >
-            </Marker>
-
-
-            <Marker
-                key={2}
-                position={new navermaps.LatLng(37.46955246261429, 126.89905664026355)}
-                icon={{
-                    url: map_Marker,
-                    size: new navermaps.Size(50, 50),
-                    origin: new navermaps.Point(0, 0),
-                    anchor: new navermaps.Point(25, 25)
-                }}
-            >
-            </Marker>
-
-
-            <Marker
-                key={3}
-                position={new navermaps.LatLng(37.4736118303616, 126.89517717590284)}
-                icon={{
-                    url: map_Marker,
-                    size: new navermaps.Size(50, 50),
-                    origin: new navermaps.Point(0, 0),
-                    anchor: new navermaps.Point(25, 25)
-                }}
-            >
-            </Marker>
-
-
-            <Marker
-                key={4}
-                position={new navermaps.LatLng(37.47045467981596, 126.89728648444668)}
-                icon={{
-                    url: map_Marker,
-                    size: new navermaps.Size(50, 50),
-                    origin: new navermaps.Point(0, 0),
-                    anchor: new navermaps.Point(25, 25),
-                }}
-            >
-            </Marker>
-
-
-            <Marker
-                key={5}
-                position={new navermaps.LatLng(37.47167694499592, 126.90042325184629)}
-                icon={{
-                    url: map_Marker,
-                    size: new navermaps.Size(50, 50),
-                    origin: new navermaps.Point(0, 0),
-                    anchor: new navermaps.Point(25, 25)
-                }}
-            >
-            </Marker>
-
-
-            <Marker
-                key={6}
-                position={new navermaps.LatLng(37.4724060587129, 126.89929374026363)}
-                icon={{
-                    url: map_Marker,
-                    size: new navermaps.Size(50, 50),
-                    origin: new navermaps.Point(0, 0),
-                    anchor: new navermaps.Point(25, 25)
-                }}
-            >
-            </Marker>
-                
             </NaverMap>
         </div>
     );
